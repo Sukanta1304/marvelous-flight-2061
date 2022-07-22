a486e4644bf904bcac3104166851d2402690696e
Sukanta1304 <101576636+Sukanta1304@users.noreply.github.com>
2022-07-22 21:55:17 +0530

    Update Roundtrip.jsx

--- marvelous-flight-2061/src/Components/Roundtrip.jsx
+++ marvelous-flight-2061/src/Components/Roundtrip.jsx
@@ -1,14 +1,18 @@
 import { Button } from '@chakra-ui/react';
 import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import Image from '../assets/Img-1.jpg'
+import { Appcontext } from '../Context/Appcontext';
 
 const Roundtrip = () => {
+  const [data]= useContext(Appcontext) ;
   return (
     <div>
         <img src={Image} alt="Roundtrip"  />
-        <Button width="500px" height="50px" bg="white" borderColor="gray.400" mt="-370px">Pick up city,Airport, Address  or hotel</Button>
+        <Button width="500px" height="50px" bg="white" borderColor="gray.400" mt="-370px"><Link to="/startpoint">{data.startpoint.length>1? data.startpoint : "Pickup City,Airport,Address or Hotel"}</Link></Button>
         <div>
-          <Button width="500px" height="50px" bg="gray.200" mt="-300px">FIND CARS</Button>
+          <Button width="500px" height="50px" bg="gray.200" mt="-300px"><Link to="/findcars">FIND CARS</Link></Button>
         </div>
         
     </div>
