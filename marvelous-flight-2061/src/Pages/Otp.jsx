56d5a59cec71b2b935d3f9a0bba88fcb557569a8
Sukanta1304 <101576636+Sukanta1304@users.noreply.github.com>
2022-07-22 09:26:59 +0530

    Update Otp.jsx

--- marvelous-flight-2061/src/Pages/Otp.jsx
+++ marvelous-flight-2061/src/Pages/Otp.jsx
@@ -1,10 +1,12 @@
 import { Button, Input } from '@chakra-ui/react'
-import React from 'react'
+import React, { useContext } from 'react'
 import { useEffect } from 'react';
 import { useState } from 'react'
 import { Navigate, useNavigate } from 'react-router-dom';
+import { Appcontext } from '../Context/Appcontext';
 
 const Otp = () => {
+    const [data] = useContext(Appcontext);
     const [time,setTime] = useState(30) ;
     const[otp,setOtp]= useState("") ;
     const navigate= useNavigate() ;
@@ -29,8 +31,9 @@ const Otp = () => {
     <div>
         <h1>
         Enter 6-digit OTP
-        OTP was sent to +919635289745
+        OTP was sent to +91{data.mobile}
         </h1>
+        <br />
         <Input placeholder='OTP'
         value={otp}
         onChange={(e)=>setOtp(e.target.value)}
