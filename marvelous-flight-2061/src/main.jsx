43d863bf4d1a4ae63131f573a41d76bf2b1dc9aa
Sukanta1304 <101576636+Sukanta1304@users.noreply.github.com>
2022-07-22 09:21:02 +0530

    Update main.jsx

--- marvelous-flight-2061/src/main.jsx
+++ marvelous-flight-2061/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App'
 import './index.css'
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
+import AppcontextProvider from './Context/Appcontext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
     <ChakraProvider>
+    <AppcontextProvider>
     <App />
+    </AppcontextProvider>
     </ChakraProvider>
     </BrowserRouter>
   </React.StrictMode>
