29da5e901fe997d900ffd68cb8fbb58d8a4d98cd
Sukanta1304 <101576636+Sukanta1304@users.noreply.github.com>
2022-07-23 21:01:00 +0530

    Update Profile.jsx

--- marvelous-flight-2061/src/Pages/Profile.jsx
+++ marvelous-flight-2061/src/Pages/Profile.jsx
@@ -1,10 +1,23 @@
 import React, { useContext } from 'react'
 import { Appcontext } from '../Context/Appcontext';
 import { PhoneIcon , ArrowForwardIcon } from '@chakra-ui/icons' ;
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import {ArrowBackIcon} from '@chakra-ui/icons';
+
+
 
 const Profile = () => {
     const [data,setData] = useContext(Appcontext);
+
+    const Userdata=()=>{
+      return(
+        <div style={{backgroundColor:"gray" , padding:"2rem"}}>
+        <h1>{data.name}</h1>
+        <h3>{data.email}</h3>
+        <h3>{data.mobile}</h3>
+        </div>
+      )
+    }
 
     const handleLogout=()=>{
         setData(
@@ -19,11 +32,8 @@ const Profile = () => {
 
   return (
     <div>
-        <div style={{backgroundColor:"gray" , padding:"2rem"}}>
-        <h1>{data.name}</h1>
-        <h3>{data.email}</h3>
-        <h3>{data.mobile}</h3>
-        </div>
+      <Link to="/"><ArrowBackIcon/></Link>
+        {(data.name!==""&& data.email!==""&& data.mobile!=="")? <Userdata/>:""}
         <br />
         <div style={{display:"flex" , gap:"1rem"}}>
             <button><Link to="/changecity">Change City</Link></button>
@@ -34,7 +44,7 @@ const Profile = () => {
         <PhoneIcon/> <h3><a href="https://www.zoomcar.com/faq"> Help and Support </a></h3>
         </div>
         <div style={{display:"flex" , gap:"1rem"}}>
-        <ArrowForwardIcon/> <button onClick={handleLogout}>Logout</button>
+        <ArrowForwardIcon/> <button onClick={handleLogout}>{data.name.length==0?"Login/signup":"Logout"}</button>
         </div>
         
     </div>
